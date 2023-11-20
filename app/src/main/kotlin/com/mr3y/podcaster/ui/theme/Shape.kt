aa261bff058f187066be8ef4c50801d65c54d099
MR3Y <abdonasr379@gmail.com>
2023-11-21 00:16:20 +0200

    Update theme

---
+++ app/src/main/kotlin/com/mr3y/podcaster/ui/theme/Shape.kt
@@ -0,0 +1,11 @@
+package com.mr3y.podcaster.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(8.dp),
+    large = RoundedCornerShape(0.dp)
+)
