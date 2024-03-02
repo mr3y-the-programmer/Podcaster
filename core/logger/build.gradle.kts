0ac08417cc3cfc62ff5d862b4d5bc9cf71830c77
MR3Y <abdonasr379@gmail.com>
2024-03-02 15:53:41 +0200

    Split logger into separate module

---
+++ core/logger/build.gradle.kts
@@ -0,0 +1,60 @@
+plugins {
+    alias(libs.plugins.com.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.ktlint)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.ksp)
+}
+
+android {
+    namespace = "com.mr3y.podcaster.core.logger"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 26
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro",
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        compose = false
+        buildConfig = false
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+ktlint {
+    filter {
+        exclude("**/generated/**")
+        exclude("**/build/**")
+    }
+}
+
+dependencies {
+    ksp(libs.hilt.compiler)
+    implementation(libs.hilt.runtime)
+
+    implementation(libs.kermit)
+    implementation(libs.kermit.crashlytics)
+}
