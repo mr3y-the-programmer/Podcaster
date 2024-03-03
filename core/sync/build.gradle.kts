9631159998c5f2379b426f7be8b07dc0a0d27902
MR3Y <abdonasr379@gmail.com>
2024-03-03 16:52:19 +0200

    Split Sync Worker into separate module

---
+++ core/sync/build.gradle.kts
@@ -0,0 +1,63 @@
+plugins {
+    alias(libs.plugins.com.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.ktlint)
+}
+
+android {
+    namespace = "com.mr3y.podcaster.core.sync"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 26
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro",
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        compose = false
+        buildConfig = false
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+ktlint {
+    filter {
+        exclude("**/generated/**")
+        exclude("**/build/**")
+    }
+}
+
+dependencies {
+    implementation(projects.core.model)
+    implementation(projects.core.data)
+    ksp(libs.hilt.compiler)
+    ksp(libs.hilt.androidx.compiler)
+    implementation(libs.hilt.common)
+    implementation(libs.hilt.runtime)
+    implementation(libs.hilt.workmanager)
+    implementation(libs.workmanager.core)
+}
