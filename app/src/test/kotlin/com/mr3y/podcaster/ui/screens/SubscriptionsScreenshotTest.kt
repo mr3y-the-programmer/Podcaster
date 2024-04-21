3c562c887ab7285f94e6d18353478890b6c992d9
M R 3 Y <26522145+mr3y-the-programmer@users.noreply.github.com>
2024-04-21 11:54:59 +0200

    Add Screenshot test for SubscriptionsScreen (#43)

---
+++ app/src/test/kotlin/com/mr3y/podcaster/ui/screens/SubscriptionsScreenshotTest.kt
@@ -0,0 +1,57 @@
+package com.mr3y.podcaster.ui.screens
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.mr3y.podcaster.core.sampledata.Episodes
+import com.mr3y.podcaster.core.sampledata.EpisodesWithDownloadMetadata
+import com.mr3y.podcaster.core.sampledata.Podcasts
+import com.mr3y.podcaster.ui.presenter.subscriptions.SubscriptionsUIState
+import com.mr3y.podcaster.ui.theme.PodcasterTheme
+import org.junit.Test
+import org.junit.experimental.categories.Category
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+@Category(ScreenshotTests::class)
+class SubscriptionsScreenshotTest : BaseScreenshotTest() {
+
+    @Test
+    fun compact_subscriptionsScreen() {
+        composeRule.setContent {
+            PodcasterTheme(dynamicColor = false) {
+                SubscriptionsScreen(
+                    state = SubscriptionsUIState(
+                        isSubscriptionsLoading = false,
+                        isEpisodesLoading = false,
+                        isRefreshing = false,
+                        refreshResult = null,
+                        subscriptions = Podcasts,
+                        episodes = EpisodesWithDownloadMetadata,
+                        queueEpisodesIds = Episodes.take(2).map { it.id },
+                    ),
+                    onPodcastClick = {},
+                    onEpisodeClick = { _, _ -> },
+                    onSettingsClick = {},
+                    onToggleAppTheme = {},
+                    onNavDrawerClick = {},
+                    onRefresh = {},
+                    onRefreshResultConsumed = {},
+                    onPlayEpisode = {},
+                    onPause = {},
+                    onAddEpisodeToQueue = {},
+                    onRemoveEpisodeFromQueue = {},
+                    externalContentPadding = PaddingValues(0.dp),
+                    excludedWindowInsets = null,
+                    currentlyPlayingEpisode = null,
+                    onConsumeErrorPlayingStatus = {},
+                    modifier = Modifier.fillMaxSize(),
+                )
+            }
+        }
+
+        takeScreenshot()
+    }
+}
